309a186e0904b4a1c128f4553b1f246d29138f97
vishveshshukla0008 <vishveshshukla82@gmail.com>
2026-03-10 20:16:31 +0530

    Hey This shit is done

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/auth";
+const API_URL = "https://movie-app-bqul.onrender.com/api/auth";
 
 const api = axios.create({
     baseURL: API_URL,
